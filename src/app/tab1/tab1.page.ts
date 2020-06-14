4b33627cff7e85e5530de4f522175d365c175edd
attilakaradeniz <attilakaradeniz@gmail.com>
2020-06-15 00:52:24 +0200

    first ionic tinkering

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  
+ 
 
-  constructor() {}
+
+  public points:number;
+  public pointsToAdd:number;
+
+  constructor(private loadingController: LoadingController) {
+    this.points = 0;
+  }
+  addOnePoint(){
+    this.points++;
+  }
+
+  subOnePoint(){
+    this.points--;
+  }
+
+  underline(){
+
+  }
+
+ 
+  async presentLoadingWithOptions(){
+    const loading = await this.loadingController.create({
+      spinner:'bubbles',
+      duration: 2000,
+      message: "Please eat . . . <br/> I'll give you 3 sec . . .",
+      translucent: true});
+       return await loading.present();
+      
+  }
+
+
+
+  
+  // addPoints(){
+  //   this.points = this.points+this.pointsToAdd+0;
+  // }
+   ngOnInit(){
+
+  }
+
+
 
 }
